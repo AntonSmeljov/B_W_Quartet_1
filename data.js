43ccfb798eca4ffe4933c73f0166f49fb84aa388
AntonSmeljov <antonsmeljov@gmail.com>
2024-12-27 16:59:04 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,569 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dnevni-borovak",
+      "name": "Dnevni borovak",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.2859313205206773,
+        "pitch": 0.05178389653195836,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8060127874506087,
+          "pitch": 0.057472656032389935,
+          "rotation": 0,
+          "target": "3-hodnik-3"
+        },
+        {
+          "yaw": -1.416941306990383,
+          "pitch": 0.08026788062236001,
+          "rotation": 0,
+          "target": "11-terasa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hodnik-1",
+      "name": "Hodnik 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.1517098498802874,
+        "pitch": 0.10235830577531857,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9124885950487265,
+          "pitch": 0.4576193567349698,
+          "rotation": 0,
+          "target": "2-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-hodnik-2",
+      "name": "Hodnik 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.22895204016618464,
+        "pitch": 0.0703455049430115,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2156915451697579,
+          "pitch": 0.12668173508620661,
+          "rotation": 0,
+          "target": "10-spavaca-soba-3"
+        },
+        {
+          "yaw": 1.5731075334134825,
+          "pitch": 0.15419337947511025,
+          "rotation": 0,
+          "target": "7-kupatilo-3"
+        },
+        {
+          "yaw": -2.985365649204832,
+          "pitch": 0.23547516544958746,
+          "rotation": 0,
+          "target": "1-hodnik-1"
+        },
+        {
+          "yaw": -0.4694344385985634,
+          "pitch": 0.5183260595298016,
+          "rotation": 0,
+          "target": "3-hodnik-3"
+        },
+        {
+          "yaw": -0.4763433444715446,
+          "pitch": 0.1094524982071654,
+          "rotation": 0,
+          "target": "9-spavaca-soba-2"
+        },
+        {
+          "yaw": -0.16994044694670407,
+          "pitch": 0.1515367845982034,
+          "rotation": 0,
+          "target": "0-dnevni-borovak"
+        },
+        {
+          "yaw": -0.7498946706277252,
+          "pitch": 0.15724834634078455,
+          "rotation": 0,
+          "target": "4-hodnik-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hodnik-3",
+      "name": "Hodnik 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.4462276361012556,
+        "pitch": 0.1335960326085317,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0315477565623805,
+          "pitch": 0.24884866953290619,
+          "rotation": 0,
+          "target": "0-dnevni-borovak"
+        },
+        {
+          "yaw": -0.3684010020254114,
+          "pitch": 0.1657710880162533,
+          "rotation": 0,
+          "target": "9-spavaca-soba-2"
+        },
+        {
+          "yaw": 2.793723912093406,
+          "pitch": 0.43194209483372425,
+          "rotation": 0,
+          "target": "2-hodnik-2"
+        },
+        {
+          "yaw": 2.90723591617658,
+          "pitch": 0.06887171825142602,
+          "rotation": 0,
+          "target": "1-hodnik-1"
+        },
+        {
+          "yaw": -1.8434910117424224,
+          "pitch": 0.24163195841731522,
+          "rotation": 0,
+          "target": "4-hodnik-4"
+        },
+        {
+          "yaw": -2.1360836931079312,
+          "pitch": 0.10083250466496807,
+          "rotation": 0,
+          "target": "6-kupatilo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hodnik-4",
+      "name": "Hodnik 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.7973256521517325,
+        "pitch": 0.06276798764906566,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9323500908438547,
+          "pitch": 0.2214619038631156,
+          "rotation": 0,
+          "target": "8-spavaca-soba-1"
+        },
+        {
+          "yaw": 1.686077382616478,
+          "pitch": 0.1276926671517966,
+          "rotation": 0,
+          "target": "5-kupatilo-1"
+        },
+        {
+          "yaw": -0.5994594983349124,
+          "pitch": 0.1698028495010675,
+          "rotation": 0,
+          "target": "6-kupatilo-2"
+        },
+        {
+          "yaw": -1.0684046767781439,
+          "pitch": 0.32886922587064227,
+          "rotation": 0,
+          "target": "3-hodnik-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-kupatilo-1",
+      "name": "Kupatilo 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.5593053414373141,
+        "pitch": 0.08099674523056066,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.547534093625252,
+          "pitch": 0.15193894582067458,
+          "rotation": 0,
+          "target": "4-hodnik-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kupatilo-2",
+      "name": "Kupatilo 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.403049752385094,
+        "pitch": 0.43542510161377734,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0603072407806975,
+          "pitch": 0.09146894902578495,
+          "rotation": 0,
+          "target": "3-hodnik-3"
+        },
+        {
+          "yaw": 2.511591561135064,
+          "pitch": 0.15483193895050285,
+          "rotation": 0,
+          "target": "4-hodnik-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kupatilo-3",
+      "name": "Kupatilo 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.10429218110690641,
+        "pitch": 0.4303824676860213,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9521333323567056,
+          "pitch": 0.10456638343622515,
+          "rotation": 0,
+          "target": "10-spavaca-soba-3"
+        },
+        {
+          "yaw": 2.471628377153336,
+          "pitch": 0.18001779884940916,
+          "rotation": 0,
+          "target": "2-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-spavaca-soba-1",
+      "name": "Spavaca soba 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.7661610313100464,
+        "pitch": 0.028480799908980003,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1284569228119006,
+          "pitch": 0.13672307153025542,
+          "rotation": 0,
+          "target": "4-hodnik-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-spavaca-soba-2",
+      "name": "Spavaca soba 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.9970493447036155,
+        "pitch": 0.011946423222699565,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9986246260718694,
+          "pitch": 0.10301999309028531,
+          "rotation": 0,
+          "target": "3-hodnik-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-spavaca-soba-3",
+      "name": "Spavaca soba 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.7702838927278064,
+        "pitch": 0.0312666052566648,
+        "fov": 1.4390866472079973
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8082825011458343,
+          "pitch": 0.20415533140945286,
+          "rotation": 0,
+          "target": "2-hodnik-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-terasa",
+      "name": "Terasa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7845361226051564,
+          "pitch": 0.011951687304188496,
+          "rotation": 0,
+          "target": "0-dnevni-borovak"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BW-QUARTET-1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
